594b90e8bd96752de4da188476473d1a6d662ac7
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-22 16:02:23 -0500

    Initial Commit

---
+++ Stockz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Stockz
+//
+//  Created by Edwin Cardenas on 22/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
